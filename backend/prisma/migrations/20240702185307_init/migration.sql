3e2498212cb5223a00c4a646051c182077f24501
Alexandre Borges de Albuquerque <103118263+axdborges@users.noreply.github.com>
2024-07-26 09:55:32 -0300

    Delete backend directory

--- backend/prisma/migrations/20240702185307_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "city" TEXT NOT NULL,
-    "country" TEXT NOT NULL,
-    "favorite_sport" TEXT NOT NULL
-);
--- backend/src/routes/Users.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import UsersController from '../controllers/Users.controller';
-
-const UserRouter = Router();
-
-UserRouter.get('', UsersController.list);
-
-export default UserRouter;
